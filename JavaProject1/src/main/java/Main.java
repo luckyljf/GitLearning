170a011f1ff0d7b62afa2c0017e59d7b8616a9ed
ljf <luckylijf@gmail.com>
2022-01-13 16:13:57 +0800

    提交Class Main

---
+++ JavaProject1/src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: ljf
+ * @Create: 2022/1/13 16:11
+ * @Description:
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("project1");
+    }
+}
