94e28903c6c8fb73d3cf79450a711b73f672e26e
ljf <luckylijf@gmail.com>
2022-01-14 14:28:51 +0800

    第一版

---
+++ JavaProject1/src/main/java/Edition.java
@@ -0,0 +1,12 @@
+/**
+ * @Author: ljf
+ * @Create: 2022/1/14 14:28
+ * @Description:
+ **/
+public class Edition {
+
+    public static void main(String[] args) {
+
+        System.out.println("第一版");
+    }
+}
