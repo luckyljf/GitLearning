f1adf60df5b9900b2934f828e9cf08f08e43b107
ljf <luckylijf@gmail.com>
2022-01-17 10:15:13 +0800

    AAA

---
+++ JavaProject1/src/main/java/EditionCommit.java
@@ -0,0 +1,12 @@
+/**
+ * @Author: ljf
+ * @Create: 2022/1/17 10:14
+ * @Description:
+ **/
+public class EditionCommit {
+
+    public static void main(String[] args) {
+
+        System.out.println("AAA");
+    }
+}
